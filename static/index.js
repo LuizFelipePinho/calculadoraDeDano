ba2625516f9af8036f4ff237c5c5c56160be24bd
luizFelipePinho <luizfelipesilva458@gmail.com>
2021-06-17 23:28:11 -0300

    adicionando validacao para selecionar um personagem

--- static/index.js
+++ static/index.js
@@ -1,9 +1,28 @@
 itens = document.querySelectorAll(".item")
 
+vidaPersonagens = []
 
 for (const item of itens){
     item.addEventListener("click", function(){
-        item.style.background = "red"
+        
+        var nome = this.getAttribute("data-nome"); // pega o valor nome
+        var vida = this.getAttribute("data-vida") // pega o valor da vida
+        if (vidaPersonagens.length == 0){  // se n for
+            if (nome == "humano" || nome == "slime" || nome == "lizardmen"){
+                vidaPersonagens.push(vida) /// joga pra lista o valor da vida
+                item.style.background = "red";
+                console.log(vida)
+                console.log(vidaPersonagens)
+            }
+        }
+        
+
     })
 }
 
+
+botao = document.getElementById("calcular")
+
+botao.addEventListener("click", function(){
+    
+})
