38e0cbfdbb8004775f23e8c491162f0aac929f3d
luizFelipePinho <luizfelipesilva458@gmail.com>
2021-06-17 23:34:12 -0300

    adicionando comentarios ao js

--- static/index.js
+++ static/index.js
@@ -1,18 +1,20 @@
-itens = document.querySelectorAll(".item")
+itens = document.querySelectorAll(".item") // pega todos os elementos com a classe item
 
-vidaPersonagens = []
+vidaPersonagens = [] // crio uma lista vazia
 
-for (const item of itens){
-    item.addEventListener("click", function(){
+for (const item of itens){ // percorre por cada indice, no caso elementos html com a classe item
+    item.addEventListener("click", function(){ // adiciona um evento junto a uma função a cada elemento html
         
         var nome = this.getAttribute("data-nome"); // pega o valor nome
         var vida = this.getAttribute("data-vida") // pega o valor da vida
-        if (vidaPersonagens.length == 0){  // se n for
+        
+        if (vidaPersonagens.length == 0){  // se não tiver nenhum elemento adiciona e colore o fundo
+            
             if (nome == "humano" || nome == "slime" || nome == "lizardmen"){
                 vidaPersonagens.push(vida) /// joga pra lista o valor da vida
-                item.style.background = "red";
-                console.log(vida)
-                console.log(vidaPersonagens)
+                item.style.background = "red"; // muda o fundo pra vermelho 
+                console.log(vida) // mostra no console so pra teste
+                console.log(vidaPersonagens) // mostra no console pra teste
             }
         }
         
@@ -23,6 +25,7 @@ for (const item of itens){
 
 botao = document.getElementById("calcular")
 
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ 
+    alert('botao funcional')
     
 })
