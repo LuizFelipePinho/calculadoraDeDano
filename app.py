4118fec02a26ef9272a7db013026445ed71ae4b0
luizFelipePinho <luizfelipesilva458@gmail.com>
2021-06-18 19:49:08 -0300

    validaçao de click

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from logging import debug
 from flask import Flask, render_template
 
 app = Flask(__name__)
--- static/index.js
+++ static/index.js
@@ -11,12 +11,23 @@ for (const item of itens){ // percorre por cada indice, no caso elementos html c
         if (vidaPersonagens.length == 0){  // se não tiver nenhum elemento adiciona e colore o fundo
             
             if (nome == "humano" || nome == "slime" || nome == "lizardmen"){
+                vidaPersonagens.push(nome)
                 vidaPersonagens.push(vida) /// joga pra lista o valor da vida
                 item.style.background = "red"; // muda o fundo pra vermelho 
                 console.log(vida) // mostra no console so pra teste
                 console.log(vidaPersonagens) // mostra no console pra teste
             }
-        }
+        }else {
+            for (i = 0; i < vidaPersonagens.length; i++){
+                if (vidaPersonagens[0] == nome){
+                    console.log(nome + " está dentro da lista")
+                }
+            
+            }
+
+        } 
+            
+        
         
 
     })
