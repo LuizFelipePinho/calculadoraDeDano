08f36776dda033c219ae1a54c5482c45b7e91462
luizFelipePinho <luizfelipesilva458@gmail.com>
2021-06-19 12:09:40 -0300

    funcionalidade de selecionar arma adicionada

--- static/index.js
+++ static/index.js
@@ -1,10 +1,12 @@
-itens = document.querySelectorAll(".item") // pega todos os elementos com a classe item
-
+personagens = document.querySelectorAll(".personagem") // pega todos os elementos com a classe personagem
+armas = document.querySelectorAll(".arma")
 
 personagemSelecionado = [] // crio uma lista vazia
+armaSelecionada = []
 
-for (const item of itens){ // percorre por cada indice, no caso elementos html com a classe item
-    item.addEventListener("click", function(){ // adiciona um evento junto a uma função a cada elemento html
+
+for (const personagem of personagens){ // percorre por cada indice, no caso elementos html com a classe personagem
+    personagem.addEventListener("click", function(){ // adiciona um evento junto a uma função a cada elemento html
         
         var nome = this.getAttribute("data-nome"); // pega o valor nome
         var vida = this.getAttribute("data-vida") // pega o valor da vida
@@ -14,7 +16,7 @@ for (const item of itens){ // percorre por cada indice, no caso elementos html c
         if (nome == "humano" || nome == "slime" || nome == "lizardmen"){
             personagemSelecionado.push(nome)  // joga pra lista o valor de nome
             personagemSelecionado.push(vida) /// joga pra lista o valor da vida
-            item.style.background = "red"; // muda o fundo pra vermelho 
+            personagem.style.background = "red"; // muda o fundo pra vermelho 
         }
     }else { // caso tenha algum elemento vc vai verificar se o personagem selecionado já foi selecioado
         
@@ -22,7 +24,7 @@ for (const item of itens){ // percorre por cada indice, no caso elementos html c
             
             if (personagemSelecionado[0] == nome){ // caso o personagem já esteja selecionado e foi clicado novamente, ent desmarca
                 removidos =  personagemSelecionado.splice(0,2) // remove  2 elementos a partir do 0 
-                item.style.background = "rgb(245, 158, 172)";
+                personagem.style.background = "rgb(245, 158, 172)";
             }   
             
         }
@@ -31,7 +33,40 @@ for (const item of itens){ // percorre por cada indice, no caso elementos html c
             
         
         
+    })
+}
 
+// A PARTIR DAQUI REPETIMOS NOSSO  CÓDIGO ACIMA PRA PEGAR A ARMA PQ N CONSIGUI REUTILIZAR O CÓDIGO :/ KKK 
+
+for (const arma of armas){ // percorre por cada indice, no caso elementos html com a classe personagem
+    arma.addEventListener("click", function(){ // adiciona um evento junto a uma função a cada elemento html
+        
+        var nomeArma = this.getAttribute("data-nome"); // pega o valor nome
+        var ataque = this.getAttribute("data-ataque") // pega o valor da vida
+        console.log(nomeArma, ataque)
+        
+    if (armaSelecionada.length == 0){  // se nenhum personagem for selecionado, adicione e colore o fundo
+        
+        if (nomeArma == "tridente" || nomeArma == "katana" || nomeArma == "espada"){
+            armaSelecionada.push(nomeArma)  // joga pra lista o valor de nome
+            armaSelecionada.push(ataque) /// joga pra lista o valor da vida
+            arma.style.background = "red"; // muda o fundo pra vermelho 
+        }
+    }else { // caso tenha algum elemento vc vai verificar se o personagem selecionado já foi selecioado
+        
+        for (i = 0; i < armaSelecionada.length; i++){ // vai percorrer a lista
+            
+            if (armaSelecionada[0] == nomeArma){ // caso o personagem já esteja selecionado e foi clicado novamente, ent desmarca
+                removidos =  armaSelecionada.splice(0,2) // remove  2 elementos a partir do 0 
+                arma.style.background = "rgb(245, 158, 172)";
+            }   
+            
+        }
+        
+    } 
+            
+        
+        
     })
 }
 
